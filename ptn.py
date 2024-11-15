cc566bf8fc406f4c918a959d613b11ed7df9a870
Ashish Kushwaha <ashishkush406@gmail.com>
2024-11-15 15:30:24 +0530

    pattrn

--- ptn.py
+++ ptn.py
@@ -1,8 +1,8 @@
-# # n=int(input("Enter No: "))
-# # i=1
-# # while i<=n:
-# #     print('*' *i + (n-i)* '  ')
-# #     i=i+1
+# n=int(input("Enter No: "))
+# i=1
+# while i<=n:
+#     print('*' *i + (n-i)* '  ')
+#     i=i+1
 
 
 
@@ -31,8 +31,80 @@
 
 
 
+# n=int(input("Enter No: "))
+# i=1
+# while i<n:
+#     print(' '*i+(n-i)*' *')
+#     i=i+1
+
+
+# n=int(input("Enter No: "))
+# i=1
+# while i<=n:
+#     print('*' *i + (n-i)* '  ')
+#     i=i+1
+# # print (i)
+# i=i-2
+# while i>0:
+#     print('*'*i)
+#     i=i-1
+
+# output
+
+
+
+
+# return star
+
+# n=int(input("Enter No: "))
+# i=1
+# while i<=n:
+#     print(' '* (n-i)+'*'*i)
+#     i=i+1
+# # print (i)
+# i=i-2
+# while i>0:
+#     print((n-i)*' '+'*'*i)
+#     i=i-1
+
+
+# output
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+
+
+
+
 n=int(input("Enter No: "))
-i=1
-while i<n:
-    print(' '*i+(n-i)*' *')
+i=n
+while i>0:
+    print('*'*i)
+    i=i-1
+# print (i)
+i=i=2
+while i<=n:
+    print('*'*i)
     i=i+1
+
+
+#  output
+
+# *****
+# ****
+# ***
+# **
+# *
+# **
+# ***
+# ****
+# *****
+
